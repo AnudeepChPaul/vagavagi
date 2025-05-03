4bad5ea5a35219901eb9398c48c0273973b81911
Anudeep Chandra Paul <me@anudeep.pro>
2025-05-04 01:13:13 +0530

    update: deps update

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { AuthenticatedApp } from 'src/context/AuthContext';
 import { DeviceLayerContext } from 'src/context/StatusBarContext';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { initiateListenrs } from 'src/util/ResponsiveUtil';
 import { RootNavigationController } from 'src/pages/auth/Root';
+import { enableMapSet } from "immer"
+
+enableMapSet();
 
 export default function App() {
   React.useEffect(() => {
@@ -14,11 +16,9 @@ export default function App() {
 
   return <SafeAreaProvider>
     <DeviceLayerContext>
-      <AuthenticatedApp>
-        <NavigationContainer>
-          <RootNavigationController />
-        </NavigationContainer>
-      </AuthenticatedApp>
+      <NavigationContainer>
+        <RootNavigationController />
+      </NavigationContainer>
     </DeviceLayerContext>
   </SafeAreaProvider>
 }
